e7bbd7640950c18653d0325fdb5cb18acbfda33a
eugenio4a <chaplovutsiizenya@gmail.com>
2021-04-13 20:55:55 +0300

    all

---
+++ app.js
@@ -0,0 +1,11 @@
+//slider review
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+})
+
+//accordion
+
+$(document).ready(function() {
+    $('.collapsible').collapsible();
+});
