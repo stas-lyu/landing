f65e9587d1dd3701e51955f5284ce4f4ec0616bd
IRUBAKAI <65865941+IRUBAKAI@users.noreply.github.com>
2021-04-15 12:05:25 +0300

    Web Project

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-$(document).ready(function () {
-    $('.carousel').carousel({
-        fullWidth: true
-    });
-
-    $('.carousel.carousel-slider').carousel({
+   $('.carousel.carousel-slider').carousel({
         fullWidth: true,
         indicators: true
     })
@@ -12,24 +7,39 @@ $(document).ready(function () {
     $('.collapsible').collapsible();
     $('.reviews').carousel({ indicators: true });
     $('.comfort_carousel').carousel({ indicators: true, fullWidth: true, })
-    //
-    // let products = document.querySelectorAll('.product_slider');
-    // let modalContent = document.querySelector('.modal-content');
-    //
-    // products.forEach((prod) => {
-    //     prod.addEventListener('click', ()=> {
-    //             let img = prod.querySelector('.product_img').src;
-    //             let title = prod.querySelector('.product_title').innerText;
-    //             let price = prod.querySelector('.product_price').innerText;
-    //             modalContent.innerHTML = `
-    //             <img src="${img}" alt="">
-    //             <h3 class="product_title">${title}</h3>
-    //             <p class="product_price">${price}</p>
-    //             `
-    //     })
-        // modalBtn.addEventListener('click', () => {
-        //     let img = prod.querySelector('.product_img').src;
-        //     console.log(img);
-        // })
-    // })
-});
+
+    $(document).ready(function () {
+        $('.carousel').carousel({
+            fullWidth: true
+        });
+        $('.modal').modal();
+
+        const products = document.querySelectorAll('.product_slider');
+        const modalImg = document.querySelector('.modal_img');
+        const modalText = document.querySelector('.modal_ul');
+
+        products.forEach((prod) => {
+            prod.addEventListener('click', ()=> {
+                const img = prod.querySelector('.product_img').src;
+                const title = prod.querySelector('.product_title').innerText;
+                const price = prod.querySelector('.product_price').innerText;
+                const productInfo = prod.querySelector('.product_info').innerText;
+                const productArticul = prod.querySelector('.articul').innerText;
+                const productColor = prod.querySelector('.product_color').innerText;
+                const productSize = prod.querySelector('.size').innerText;
+                modalImg.innerHTML = `<img src="${img}" alt="">`
+                modalText.innerHTML = `
+                    <h4>${productInfo}</h4>
+                    <ul>
+                        <li>${productArticul}</li>
+                        <li>${title}</li>
+                        <li>${productColor}</li>
+                        <li>${productSize}</li>
+                        <li>${price}</li>
+                    </ul>
+                    
+                    
+                `
+            })
+        })
+    });
